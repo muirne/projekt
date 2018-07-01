59a630ad45440697f9b5c96278fcd15cbfaad54c
muirne22@hotmail.com <muirne22@hotmail.com>
2018-07-01 10:03:00 +0000

    done

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'projekt');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'coderslab');
+define('DB_PASSWORD', 'bubi20');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
